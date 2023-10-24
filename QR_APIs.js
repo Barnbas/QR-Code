2565e2c3134fb6a15b00d23184e5af8c5a05dc91
Barnbas <barnbastelagareddy123@gmail.com>
2023-10-24 20:33:29 +0530

    cors

--- QR_APIs.js
+++ QR_APIs.js
@@ -8,12 +8,11 @@ const Server=require("./mongodb/Server_Logic")
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://frontend-qrcode-i3zx.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors({
+  origin: 'https://frontend-qrcode-i3zx.vercel.app', // Adjust to your frontend's URL
+  methods: 'GET, POST, PUT, DELETE, OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 
 const port = process.env.PORT;
 
