b7bd3840f1d5e8a1ce69cd4bea9a557af9e5e2d1
Barnbas <barnbastelagareddy123@gmail.com>
2023-10-24 19:41:29 +0530

    Apisdata

--- QR_APIs.js
+++ QR_APIs.js
@@ -7,6 +7,12 @@ const dotenv = require("dotenv").config();
 const Server=require("./mongodb/Server_Logic")
 app.use(bodyParser.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://frontend-qrcode-i3zx.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json());
 
 const port = process.env.PORT;
